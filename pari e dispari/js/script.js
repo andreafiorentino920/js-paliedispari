46c8355f25c2ecf869c2f5699a86be174844863f
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-09-01 10:47:52 +0100

    Esercizio completo e corretto pari e dispari

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -1,11 +1,8 @@
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
-
 var scelta = prompt("Scegliere pari o dispari");
 console.log("La scelta da te effettuata è", scelta);
+
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
 var numeroUtente = parseInt(prompt("Scegliere un numero tra 1 e 5"));
 console.log("Il numero scelto da te è",numeroUtente);
 function generaNumero(){
@@ -15,8 +12,12 @@ function generaNumero(){
 }
 var numeroCpu = generaNumero();
 console.log("Il numero generato dal computer è",numeroCpu)
+
+// Sommiamo i due numeri
 var somma = numeroUtente + numeroCpu;
 console.log("La somma è",somma);
+
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 function pariDispari(totale){
     var risultato = "";
     if (totale%2 ==0){
@@ -27,6 +28,7 @@ function pariDispari(totale){
     console.log("Il risultato è", risultato);
     return risultato;
 }
+// Dichiariamo chi ha vinto.
 if (scelta == "pari" && pariDispari(somma) == "pari"){
     console.log("Hai vinto");
 }else if(scelta == "dispari" && pariDispari(somma) == "dispari"){
